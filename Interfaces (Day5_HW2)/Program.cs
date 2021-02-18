8d47bcae9d9daa58fba7ec2777921da501d6acfd
Ege Çıtak <egecitak01@gmail.com>
2021-02-18 22:40:25 +0300

    Day_5/HW2

---
+++ Interfaces (Day5_HW2)/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Interfaces__Day5_HW2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interfaces cannot be instanced
+            //IPersonManager customerManager = new CustomerManager();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+    }
+
+    //inherits - class
+    //implements - interface
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //customer adding codes
+            Console.WriteLine("Müşteri eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi.");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
